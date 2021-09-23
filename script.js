8ab4117af78ab0b9f10aa86eb3afa06406075d63
davidscdev <davidsc.dev@gmail.com>
2021-09-23 19:13:02 -0300

    Adicionada a funcionalidade de carregar os itens do JSON pra tela.

--- script.js
+++ script.js
@@ -6,5 +6,13 @@ const cs = (el) => document.querySelectorAll(el);
 pizzaJson.map((item, index) => {
     let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true); // Cria um elemento igual ao pizza-item que está dentro da div models (essa tá hdden).
 
-    c.('.pizza-area').append(pizzaItem); //Coloca esse elemento que acabou de ser criado e coloca na div pizza-area.
+    //Carregando os itens do jSON para a tela.
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
+    c('.pizza-area').append(pizzaItem); //Coloca esse elemento que acabou de ser criado e coloca na div pizza-area.
+
+    console.log(item.name);
 });
