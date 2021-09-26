625dbf62fe182bda9085a9409e5c4780f1f90fc1
davidscdev <davidsc.dev@gmail.com>
2021-09-25 22:52:55 -0300

    Popula o modal com os dados das pizzas e reseta os botôes de tamanho e qtd das pizzas.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let modalQt = 1;
+
 //simplifica a seleção de elementos com o uso do c -> querySelector e cs -> All
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
@@ -7,6 +9,7 @@ pizzaJson.map((item, index) => {
     let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true); // Cria um elemento igual ao pizza-item que está dentro da div models (essa tá hdden).
 
     //Carregando os itens do jSON para a tela.
+    pizzaItem.setAttribute('data-key', index); //Seta o indice da pizza pra passar para o modal.
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
@@ -15,10 +18,33 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
 
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        console.log(pizzaJson[key]);
+
+        modalQt = 1;
+
+        c('.pizzaBig img').src = pizzaJson[key].img;
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+
+        cs('.pizzaInfo--size').forEach((size, index) => {
+
+            if (index == 2) {
+                size.classList.add('selected');
+            }
+
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[index];
+        });
+
+
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+        c('.pizzaInfo--actualPrice').innerHTML = `R$ ${ pizzaJson[key].price.toFixed(2)}`;
         c('.pizzaWindowArea').style.opacity = 0;
         c('.pizzaWindowArea').style.display = 'flex';
         setTimeout(() => c('.pizzaWindowArea').style.opacity = 1, 200);
-        console.log(`Clicou na pizza de ${item.name}.`);
+        console.log(`Clicou na pizza de ${key}.`);
     });
 
     c('.pizza-area').append(pizzaItem); //Coloca esse elemento que acabou de ser criado e coloca na div pizza-area.
